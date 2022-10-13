745c3ebc9b3ab3af317ed6e6ad928f2dcc0ee1e8
Javier Castaneda <castaneda.javier.j@gmail.com>
2022-10-13 15:07:23 -0500

    about to begin exercise 10

--- code.js
+++ code.js
@@ -2,21 +2,29 @@
 function helloWorld() {
     return "Hello, World!";
 }
-//sayHello function
-// function sayHello(input) {
-//     if (input === "Jane") {
-//         return "Hello, Jane!";
-//     } else if (input === "Alex") {
-//         return "Hello, Alex!"
-//     } else if (input === "Pat") {
-//         return "Hello, Pat!";
-//     } else {
-//         return '';
-//     }
-// }
 
+//sayHello function
+function sayHello(input) {
+    if (input === "Jane") {
+        return "Hello, Jane!";
+    } else if (input === "Alex") {
+        return "Hello, Alex!"
+    } else if (input === "Pat") {
+        return "Hello, Pat!";
+    } else if (input === "World") {
+        return "Hello, World!";
+    } else if (input === true) {
+        return "Hello, World!";
+    } else if (input === false) {
+        return "Hello, World!";
+    } else {
+        return '';
+    }
+}
+
+// *#8 refactor to be a SWITCH STATEMENT*
 
 //  *refactored #6*
-function sayHello(input) {
-    return "Hello, " + input + "!"
-}
+// function sayHello(input) {
+//     return "Hello, " + input + "!"
+// }
--- tests.js
+++ tests.js
@@ -32,6 +32,15 @@ describe('sayHello', function() {
     it('should return the string "Hello, Pat!" when executed', function() {
         expect(sayHello('Pat')).toBe("Hello, Pat!");
     });
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello('World')).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when true boolean', function() {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when false boolean', function() {
+        expect(sayHello(false)).toBe("Hello, World!");
+    });
     it("should never return 'undefined' when called", function() {
         expect(sayHello()).not.toBe(undefined);
     });
